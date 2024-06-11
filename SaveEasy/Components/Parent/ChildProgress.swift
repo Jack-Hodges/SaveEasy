05f95b8864c5b7d534789355080ce0e9ae1f7f88
Jack Hodges <jackhodg3s@gmail.com>
2024-06-11 13:32:41 +1000

    Added ChildProgress UI to display child saving progress on parent dashboard

---
+++ SaveEasy/Components/Parent/ChildProgress.swift
@@ -0,0 +1,18 @@
+//
+//  ChildProgress.swift
+//  SaveEasy
+//
+//  Created by Jack Hodges on 11/6/2024.
+//
+
+import SwiftUI
+
+struct ChildProgress: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChildProgress()
+}
